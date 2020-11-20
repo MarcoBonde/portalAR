5c21b05b228740b6baf275d5704a387ff699ea1d
ElisaCristoderoStud <elisa_cristodero@studenti.naba.it>
2020-11-20 16:37:54 +0100

    sfx mappa e ambient

--- AR PORTAL GAME/Assets/Scripts/LensManager.cs
+++ AR PORTAL GAME/Assets/Scripts/LensManager.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class LensManager : MonoBehaviour
 {
     public GameObject lenti;
+    public AudioSource background;
+
     // Start is called before the first frame update
     void Start()
     {
+        background = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,10 +24,12 @@ public class LensManager : MonoBehaviour
         if (other.CompareTag("AccendiLenti"))
         {
             lenti.SetActive(true);
+            background.Play();
         }
         if (other.CompareTag("SpegniLenti"))
         {
             lenti.SetActive(false);
+            
         }
     }
 
