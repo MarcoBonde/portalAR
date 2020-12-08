9bfd60bdb74b23ca2897094f77f08dc2530836ce
MarcoBondesaniNaba <MarcoBondeNaba@users.noreply.github.com>
2020-12-08 13:06:36 +0100

    Background Music When Entering portal

--- AR PORTAL GAME/Assets/Scripts/LensManager.cs
+++ AR PORTAL GAME/Assets/Scripts/LensManager.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class LensManager : MonoBehaviour
 {
     public GameObject lenti;
-    public AudioSource background;
 
     // Start is called before the first frame update
     void Start()
     {
-        background = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,7 +22,6 @@ public class LensManager : MonoBehaviour
         if (other.CompareTag("AccendiLenti"))
         {
             lenti.SetActive(true);
-            background.Play();
         }
         if (other.CompareTag("SpegniLenti"))
         {
